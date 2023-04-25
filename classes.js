ae4571be017c6ab6df6e6a383adee8e33c2b354a
Monique <79199057+moniq-e@users.noreply.github.com>
2023-04-25 11:38:56 -0300

    adicionar itens no inventário

--- classes.js
+++ classes.js
@@ -7,8 +7,9 @@ export class Enemy {
 }
 
 export class Item {
-    constructor(name, def, atk) {
+    constructor(name, category, def, atk) {
         this.name = name
+        this.category = category
         this.def = def
         this.atk = atk
     }
@@ -18,15 +19,11 @@ export class Item {
             let keys = Object.keys(items)
             name = keys[Math.floor(Math.random() * keys.length)]
         }
-        return items[name].clone()
+        return items[name]()
     }
 
     get image() {
         return `./images/${this.name}.png`
-    }
-
-    clone() {
-        return new Item(this.name, this.def, this.atk)
     }
 }
 
@@ -34,11 +31,68 @@ export class Item {
  * @type {{[key: string]: Item}}
  */
 const items = {
-    sword: new Item("sword", 0, 5)
+    coppershield: () => new Item("EscudoCobre", "shield", 5, 0),
+    ironshield: () => new Item("EscudoFerro", "shield", 10, 0),
+    goldenshield: () => new Item("EscudoOuro", "shield", 15, 0),
+    copperhelmet: () => new Item("CapaceteCobre", "helmet", 5, 0),
+    ironhelmet: () => new Item("CapaceteFerro", "helmet", 10, 0),
+    goldenhelmet: () => new Item("CapaceteOuro", "helmet", 15, 0),
+    copperleggings: () => new Item("CalçaCobre", "leggings", 5, 0),
+    ironleggings: () => new Item("CalçaFerro", "leggings", 10, 0),
+    goldenleggings: () => new Item("CalçaOuro", "leggings", 15, 0),
+    copperboots: () => new Item("BotaCobre", "boots", 5, 0),
+    ironboots: () => new Item("BotaFerro", "boots", 10, 0),
+    goldenboots: () => new Item("BotaOuro", "boots", 15, 0),
+    copperchest: () => new Item("PeitoralCobre", "chest", 5, 0),
+    ironchest: () => new Item("PeitoralFerro", "chest", 10, 0),
+    goldenchest: () => new Item("PeitoralOuro", "chest", 15, 0)
 }
 
 export class Player {
+    /**
+     * @type {Item[]}
+     */
+    inv
+    /**
+     * @type {Item[]}
+     */
+    equip
     constructor() {
-        //
+        this.def = 5
+        this.atk = 5
+        this.life = 20
+        this.equip = []
+        this.inv = []
+    }
+
+    getAtk() {
+        let total = this.atk
+        for (const item of this.equip) {
+            total += item.atk
+        }
+        return total
+    }
+
+    getDef() {
+        let total = this.def
+        for (const item of this.equip) {
+            total += item.def
+        }
+        return total
+    }
+
+    /**
+     * @param {Item} item
+     */
+    addEquip(item) {
+        if (this.equip.length == 8) return false
+        
+        else this.equip.push(item)
+        return true
+    }
+
+    removeItem(slot) {
+        this.inv[slot] = null
+        this.inv = this.inv.filter(e => e)
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import { Enemy, Item } from "./classes.js"
+import { Enemy, Item, Player } from "./classes.js"
 
-/**
- * @type {Item[]}
- */
-let inv = []
-let def = 10, atk = 5, life = 10
+const player = new Player()
 
 for (let i = 0; i < 24; i++) {
     let div = document.createElement("div")
@@ -13,37 +9,45 @@ for (let i = 0; i < 24; i++) {
     document.querySelector("#grid").appendChild(div)
 }
 
+for (let i = 0; i < 8; i++) {
+    let div = document.createElement("div")
+    div.id = i
+    div.appendChild(document.createElement("img"))
+    document.querySelector("#equipment").appendChild(div)
+}
+
+// BATALHA
 document.querySelector("button#battle").addEventListener("click", e => {
     change("main", "div#battleMenu")
 
-    let text = document.querySelector("div#battleMenu").querySelector("div#text")
+    let text = document.querySelector("div#battleMenu div#text")
     let enemy = new Enemy(1, 1, 1)
     let attacks = -1
 
     while (enemy.life > 0) {
         attacks++
-        enemy.life -= atk
+        enemy.life -= player.getAtk()
     }
 
     for (let i = 0; i < attacks; i++) {
-        life -= enemy.atk
+        player.life -= enemy.atk
     }
 
-    if (life > 0) {
+    if (player.life > 0) {
         text.innerText = "Você ganhou!"
-        inv.push(Item.get())
-    } else text.innerText = "Você perdeu!"
+        player.inv.push(Item.get())
+    } else {
+        text.innerText = "Você perdeu!"
+    }
 })
 
+// INVENTÁRIO
 document.querySelector("button#inventory").addEventListener("click", e => {
     change("main", "#inventoryMenu")
-
-    let menu = document.querySelector("#inventoryMenu")
-    for (let i = 0; i < inv.length; i++) {
-        menu.querySelector(`div[id='${i}']`).querySelector("img").src = inv[i].image
-    }
+    drawInv()
 })
 
+// LOJA
 document.querySelector("button#shop").addEventListener("click", e => {
     change("main", "#shopMenu")
 })
@@ -59,4 +63,22 @@ for (let b of document.querySelectorAll(".back")) {
 function change(actual, future) {
     document.querySelector(actual).style.display = "none"
     document.querySelector(future).style.display = "block"
+}
+
+for (const div of document.querySelectorAll("div#grid div")) {
+    div.addEventListener("click", e => {
+        if (player.addEquip(player.inv[div.id])) {
+            player.removeItem(div.id)
+            drawInv()
+        }
+    })
+}
+
+function drawInv() {
+    for (let i = 0; i < 24; i++) {
+        document.querySelector(`div#grid div[id='${i}'] img`).src = (player.inv[i] ? player.inv[i].image : '')
+    }
+    for (let i = 0; i < 8; i++) {
+        document.querySelector(`div#equipment div[id='${i}'] img`).src = (player.equip[i] ? player.equip[i].image : '')
+    }
 }
