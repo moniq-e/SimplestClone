cd34abcd5aac1a6bc6539bcaa3dbf6032e22e890
Monique <79199057+moniq-e@users.noreply.github.com>
2023-05-03 18:05:16 +0000

    precisa de mais teste

--- classes/enemy.js
+++ classes/enemy.js
@@ -23,11 +23,5 @@ const enemys = [
     () => new Enemy("Esqueleto", 0, 3, 10, 1),
     () => new Enemy("Zumbi", 5, 5, 10, 2),
     () => new Enemy("Aranha", 15, 10, 15, 3),
-    () => new Enemy("Grandalhao", 20, 15, 20, 4),
-    //fazer bixos
-    () => new Enemy("Esqueleto", 0, 3, 10, 1),
-    () => new Enemy("Esqueleto", 0, 3, 10, 1),
-    () => new Enemy("Esqueleto", 0, 3, 10, 1),
-    () => new Enemy("Esqueleto", 0, 3, 10, 1),
-    () => new Enemy("Esqueleto", 0, 3, 10, 1),
+    () => new Enemy("Grandalhao", 20, 15, 20, 4)
 ]
--- sections/battle.js
+++ sections/battle.js
@@ -21,24 +21,15 @@ document.querySelector("button#battle").addEventListener("click", e => {
 for (const div of document.querySelectorAll("#moblist div")) {
     div.addEventListener("click", _ => {
         let enemy = mobs[parseInt(div.id)]
-        let attacks = -1
+        
+        let playerAtk = player.getAtk() * (player.getAtk() / enemy.def > 0.95 ? 0.95 : player.getAtk() / enemy.def)
+        let enemyAtk = enemy.atk * (enemy.atk / player.def > 0.95 ? 0.95 : enemy.atk / player.def)
 
-        //debugar, arrumar e melhorar sistema de combate
-        if (player.life) {
-            while (enemy.life > 0) {
-                attacks++
-                enemy.life -= (player.getAtk() / enemy.def >= 1 ? 1 : parseInt(player.getAtk() * (1 - (player.getAtk() / enemy.def))))
+        do {
+            enemy.life -= playerAtk
+            if (enemy.life > 0) {
+                player.life -= enemyAtk
             }
-        
-            for (let i = 0; i < attacks; i++) {
-                player.life -= (enemy.atk / player.getDef() >= 1 ? 1 : parseInt(enemy.atk * (1 - (enemy.atk / player.getDef()))))
-            }
-        
-            if (player.life > 0) {
-                if (Math.random() >= 0.9) player.inv.push(Item.get())
-                player.coins += r(15, 30) * enemy.lvl
-                player.xp += r(15, 30) * enemy.lvl
-            }
-        }
+        } while (player.life > 0 || enemy.life > 0);
     })
 }
